b682aa119159d7d5085a4da813b9bb0c315a383d
Zane Fink <finkzane@gmail.com>
2025-02-22 20:45:12 -0600

    Override values on replay (#3)

--- test/test.py
+++ test/test.py
@@ -10,5 +10,16 @@ def test_marinade():
     md.mark_region_end("test_marinade")
     md.replay_region("test_marinade")
 
+def test_loop():
+    total_iters = 5
+    executed_iters = 0
+    md.mark_region_begin("test_loop")
+    for i in range(total_iters):
+        executed_iters += 1
+    print(f"executed_iters: {executed_iters}")
+    md.mark_region_end("test_loop")
+    md.replay_region("test_loop", overrides = {'total_iters': 10})
+
+
 test_marinade()
-md.replay_region("test_marinade")
+test_loop()
